2a31d99b8e8e3c2ab35b2240e9c3cfd680025072
Michael House <housem@ffbf.com>
2022-12-06 16:55:07 -0500

    Day05 partial

--- Day05.ps1
+++ Day05.ps1
@@ -122,47 +122,57 @@ function Puzzle2 {
     $baseRowLocation += 2 # The empty line between setup and instructions.
 
     for ($i = $baseRowLocation; $i -le $PuzzleInput.Length - 1; $i++) {
+        
         if ($PuzzleInput[$i].Length) {
+            Write-Host $PuzzleInput[$i]
             $inputArray = $PuzzleInput[$i].Split(' ')
             $MoveQty = $inputArray[1]
             $MoveFrom = $inputArray[3]
             $MoveTo = $inputArray[5]
-            for ($j = 0; $j -lt $MoveQty; $j++) {
-                $objectsToMove = $stacksArray | Where-Object { $_.ColumnName -eq $MoveFrom } | Sort-Object -Property StackLocation -Descending 
+            # for ($j = 0; $j -lt $MoveQty; $j++) {
+                $objectsToMove = $stacksArray | Where-Object { $_.ColumnName -eq $MoveFrom } | Sort-Object -Property StackLocation -Descending | Select-Object -First $MoveQty
                 $targetColumn = $stacksArray | Where-Object { $_.ColumnName -eq $MoveTo }
                 $targetColumnCount = 0
+                Write-Host 'objects' $objectsToMove
                 if ($null -ne $targetColumn ) {
-                    if ($null -eq $targetColumn.Count) {
-                        # If there's only one object, you get an object.
+                    if ($null -eq $targetColumn.Count) { # If there's only one object, you get an object.
                         $targetColumnCount = 1
                     }
                     else { $targetColumnCount = $targetColumn.Count } #If there's more than one object, you get an array.
                 } 
                 if ($null -ne $objectsToMove) {
-                    # if($null -eq $objectsToMove.Count) {
-                        Write-Host 'MOving ' $objectsToMove.Count
-                    # } else {
+                    if($null -eq $objectsToMove.Count) {
+                        $objectsToMove.ColumnName = $MoveTo
+                        $objectsToMove.StackLocation = $targetColumnCount
+                        
+                    } else {
+                        $objectsToMove = $objectsToMove | Sort-Object -Property StackValue -Descending
+                        Write-Host 'sorted objects' $objectsToMove
+                        Write-Host 'tc ' $targetColumnCount
                         foreach ($objectToMove in $objectsToMove) {
+                        # for($j = $objectsToMove.Count; $j -ge 0; $j--) {
+                            # Write-Host 'J count' $j
                             $objectToMove.ColumnName = $MoveTo
                             $objectToMove.StackLocation = $targetColumnCount
                             $targetColumnCount++
                         }
-                    # }
-
-                    
+                    }
                 }
-            }
+                Write-Host 'Moved ' $objectsToMove -ForegroundColor Green
+            # }
         }
 
     }
     $finalOutput = ''
     foreach ($column in $baseRowArray) {
         $TopCrate = $stacksArray | Where-Object { $_.ColumnName -eq $column.ColumnName } | Sort-Object -Property StackLocation -Descending | Select-Object  -First 1
+        # Write-Host $TopCrate
         $finalOutput += $TopCrate.StackValue
     }
+    Write-Host $stacksArray
     Write-Host 'Puzzle 2: ' $finalOutput
     return $finalOutput
 }
 
 Puzzle1 -PuzzleInput $PuzzleInput
-#Puzzle2 -PuzzleInput $PuzzleInput
+Puzzle2 -PuzzleInput $PuzzleInput
